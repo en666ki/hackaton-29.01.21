73510bef2fa40467373e6d1892c9d9582719e446
en666ki <soulpest@mail.ru>
2021-01-30 23:02:40 +0300

    Lemmatize text. Something about vectorizing

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,31 @@
-import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 import re
-import pymorphy2
+from string import punctuation
 
-f = open
+lemmatizer = WordNetLemmatizer()
+
+
+
+def LemmaTextFromFile(fname):
+    f = open(fname)
+    rtxt = f.read()
+    rtxt = re.sub(r"(<[^>]*>)([^<>]*?)(<\/[^>]*>)",'' , rtxt)
+    rtxt = re.sub(r"((http|https):\/\/)*(www)*(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}(\/*[a-zA-Z0-9]*)", "", rtxt)
+    sentences = sent_tokenize(rtxt)
+    txt = []
+    for sentence in sentences:
+        txt += [lemmatizer.lemmatize(w) for w in word_tokenize(sentence) if w not in stopwords.words('english') and w not in punctuation]
+    return txt
+
+def Similarity(lemma_text):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.metrics.pairwise import linear_kernel
+    vectorizer = TfidfVectorizer()
+    vector = vectorizer.fit_transform(lemma_text)
+    print(vector)
+
+
+lemmatext = LemmaTextFromFile('rvision-hackathon-2021-q1/marked/Mandiant_APT1_Report.pdf.txt')
+#vectorize(lemmatext)
